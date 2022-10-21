17c8c9eb20a449804d1fb4ef1e652a9b9f6e0aea
cheanith2909 <sokcheanith1031@hotmail.com>
2022-10-21 15:29:07 +0700

    add

--- test-push-notification-fcm/Program.cs
+++ test-push-notification-fcm/Program.cs
@@ -1,4 +1,4 @@
-using FirebaseAdmin;
+﻿using FirebaseAdmin;
 using Google.Apis.Auth.OAuth2;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,8 +18,15 @@ FirebaseApp.Create(new AppOptions()
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(c =>
+    {
+        c.RouteTemplate = "testpush/swagger/{documentName}/swagger.json";
+    });
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/testpush/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = "testpush/swagger";
+    });
 }
 
 app.UseHttpsRedirection();
